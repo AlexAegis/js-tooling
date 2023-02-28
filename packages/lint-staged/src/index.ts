9717cd3a776019c143e44a2d5bfcadf850da820c
AlexAegis <alexaegis@gmail.com>
2023-03-01 00:34:33 +0100

    feat: extend lint-staged js/ts step with cjs/mjs variants

--- packages/lint-staged/src/index.ts
+++ packages/lint-staged/src/index.ts
@@ -5,7 +5,7 @@ const eslintCommand = 'eslint --max-warnings=0 --no-ignore';
 const markdownCommand = 'remark --frail --no-stdout --silently-ignore';
 
 export const lintStagedConfig = {
-	'*.(ts|js)': [eslintCommand, prettierCommand],
+	'*.(ts|js|cts|cjs|mts|mjs)': [eslintCommand, prettierCommand],
 	'*.css': ['stylelint', prettierCommand],
 	'*.scss': ['stylelint --customSyntax=postcss-scss', prettierCommand],
 	'(*.json|.eslintrc|.prettierrc|.stylelintrc|.markdownlintrc)': [eslintCommand, prettierCommand],
