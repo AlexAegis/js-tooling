c805b75bd32fdc3b19a8c9f4614db99da4a75710
AlexAegis <alexaegis@gmail.com>
2023-03-07 20:16:10 +0100

    feat(lint-staged-config): add tsc check

--- packages/lint-staged-config/src/index.ts
+++ packages/lint-staged-config/src/index.ts
@@ -1,7 +1,9 @@
 // For the glob features check out https://github.com/micromatch/micromatch
+import { groupByCommonNearestFile } from '@alexaegis/fs';
+import { join } from 'node:path';
 
 const prettierCommand = 'prettier --check';
-// const tscCommand = 'tsc --noEmit';
+const tscCommand = 'tsc --noEmit';
 
 const eslintSingleFileCommand = 'eslint --max-warnings=0 --no-ignore';
 const markdownCommand = 'remark --frail --no-stdout --silently-ignore';
@@ -9,33 +11,20 @@ const markdownCommand = 'remark --frail --no-stdout --silently-ignore';
 // TODO: explore more optimal settings since eslint is slow when used on files 1 by 1
 
 export const lintStagedConfig = {
-	'**/!(shims)/*.(ts|js|cts|cjs|mts|mjs)?(x)': [eslintSingleFileCommand, prettierCommand],
+	'**/!(shims)/*.(ts|js|cts|cjs|mts|mjs)?(x)': (filenames: string[]): string[] => {
+		const groups = groupByCommonNearestFile(filenames, 'tsconfig.json');
+		return Object.keys(groups).map(
+			(tsRoot) => `${tscCommand} --project ${join(tsRoot, 'tsconfig.json')}`
+		);
+	},
+	'**/!(shims)/*.(ts|js|cts|cjs|mts|mjs)': [eslintSingleFileCommand, prettierCommand],
 	'*.css': ['stylelint', prettierCommand],
 	'*.scss': ['stylelint --customSyntax=postcss-scss', prettierCommand],
 	'(*.json|.eslintrc|.prettierrc|.stylelintrc|.markdownlintrc)': [
 		eslintSingleFileCommand,
 		prettierCommand,
 	],
+	'*.svelte': ['svelte-check', prettierCommand],
 	'*.md': [markdownCommand, prettierCommand],
 	'*.(yml|yaml)': [prettierCommand],
 };
-
-/*
-
-	'*.(ts|js|cts|cjs|mts|mjs)?(x)': (filenames: string[]): string[] => {
-		const exceptShims = filenames.filter((filename) => filename.includes('/shims/'));
-
-		const groups = groupByCommonNearestFile(exceptShims, 'tsconfig.json');
-
-		const tsCommands: string[] = [];
-		for (const [tsRoot, groupedFilenames] of Object.entries(groups)) {
-			for (const filename of groupedFilenames) {
-				tsCommands.push(
-					`${tscCommand} --project ${join(tsRoot, 'tsconfig.json')} ${filename}`
-				);
-			}
-		}
-		console.log('LMAO', groups);
-		return ['exit 1', tscCommand];
-	},
-	*/
