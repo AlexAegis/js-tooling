aefb22e2e5507256d09629decd3e4bb6b82746a2
AlexAegis <alexaegis@gmail.com>
2023-07-16 22:45:39 +0200

    feat(autotool-plugin-ts): include more in the workspace root

--- packages/lint-staged-config/src/index.ts
+++ packages/lint-staged-config/src/index.ts
@@ -10,6 +10,34 @@ const eslintSingleFileCommand = 'eslint --max-warnings=0 --no-ignore';
 const markdownCommand = 'remark --frail --no-stdout --silently-ignore';
 
 // TODO: explore more optimal settings since eslint is slow when used on files 1 by 1
+/**
+ * Peek
+ * ```js
+ *	'!(shims)/(*.(ts|js|cts|cjs|mts|mjs|jsx|tsx)|tsconfig*.json)?(x)': tsc,
+ *	'!(shims)/*.(ts|js|cts|cjs|mts|mjs)': [eslintSingleFileCommand, prettierCommand],
+ *	'*.css': ['stylelint', prettierCommand],
+ *	'*.scss': ['stylelint --customSyntax=postcss-scss', prettierCommand],
+ *	'*.(html|svelte|vue|astro|xml|php)': ['stylelint --customSyntax=postcss-html'],
+ *	'*.(html|vue|astro|xml)': [prettierCommand],
+ *	'(*.json|.eslintrc|.prettierrc|.stylelintrc|.markdownlintrc)': [
+ *		eslintSingleFileCommand,
+ *		prettierCommand,
+ *	],
+ *	'*.svelte': ['svelte-check', prettierCommand],
+ *	'*.md': [markdownCommand, prettierCommand],
+ *	'*.(yml|yaml)': [prettierCommand],
+ * ```
+ *
+ * ---
+ *
+ * ```js
+ * const prettierCommand = 'prettier --check';
+ * const tscCommand = 'tsc --noEmit';
+ *
+ * const eslintSingleFileCommand = 'eslint --max-warnings=0 --no-ignore';
+ * const markdownCommand = 'remark --frail --no-stdout --silently-ignore';
+ * ```
+ */
 export const lintStagedConfig: Config = {
 	'**/!(shims)/(*.(ts|js|cts|cjs|mts|mjs|jsx|tsx)|tsconfig*.json)?(x)': (
 		filenames: string[],
