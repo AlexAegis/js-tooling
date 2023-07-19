71b7ffc48191cf1d3cb27157363e4d8923656bfc
AlexAegis <alexaegis@gmail.com>
2023-07-19 22:11:05 +0200

    feat(eslint-config-svelte): disabled some conflicting rules

--- packages/eslint-config-svelte/src/index.ts
+++ packages/eslint-config-svelte/src/index.ts
@@ -5,11 +5,7 @@ export default {
 	overrides: [
 		{
 			files: ['*.svelte'],
-			plugins: ['@typescript-eslint'],
-			parserOptions: {
-				project: true, // For @typescript-eslint
-				extraFileExtensions: ['.svelte'],
-			},
+			parser: 'svelte-eslint-parser',
 			extends: [
 				'plugin:@typescript-eslint/recommended',
 				'plugin:@typescript-eslint/recommended-type-checked',
@@ -17,7 +13,30 @@ export default {
 				'plugin:@typescript-eslint/strict-type-checked',
 				'plugin:@typescript-eslint/stylistic',
 				'plugin:@typescript-eslint/stylistic-type-checked',
+				'plugin:svelte/recommended',
+				'plugin:svelte/prettier',
 			],
+			parserOptions: {
+				project: true, // For @typescript-eslint
+				parser: '@typescript-eslint/parser', // svelte script tags read the parser from here
+				extraFileExtensions: ['.svelte'],
+			},
+			globals: {
+				$$Generic: true, // To let `type T = $$Generic;` be defined
+			},
+			settings: {
+				svelte: {
+					// These ignores will affect only the component template, but not the script tag
+					ignoreWarnings: [
+						'@typescript-eslint/no-unsafe-assignment',
+						'@typescript-eslint/no-unsafe-member-access',
+						'@typescript-eslint/no-unsafe-call',
+						'@typescript-eslint/no-unsafe-return',
+						'@typescript-eslint/no-unsafe-argument',
+						'@typescript-eslint/no-confusing-void-expression',
+					],
+				},
+			},
 			rules: {
 				quotes: ['error', 'single', { avoidEscape: true }],
 				'@typescript-eslint/no-invalid-void-type': [
@@ -27,26 +46,13 @@ export default {
 						allowAsThisParameter: true,
 					},
 				],
-			},
-		},
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			extends: ['plugin:svelte/recommended', 'plugin:svelte/prettier'],
-			parserOptions: {
-				parser: '@typescript-eslint/parser', // svelte script tags read the parser from here
-				extraFileExtensions: ['.svelte'],
-			},
-			globals: {
-				$$Generic: true, // To let `type T = $$Generic;` be defined
-			},
-			rules: {
 				'@typescript-eslint/no-unused-vars': [
 					'warn',
 					{ varsIgnorePattern: '$$', argsIgnorePattern: '_' }, // To let $$Slot be defined
 				],
-				'@typescript-eslint/no-unsafe-assignment': 'off', // Cannot cast to $$Generic
-				'@typescript-eslint/no-unsafe-argument': 'off', // Doesn't work properly
+				'@typescript-eslint/dot-notation': 'off', // Conflicts with: "Property comes from an index signature, so it must be accessed with [].js(4111)"
+				'@typescript-eslint/no-unsafe-call': 'off',
+				'@typescript-eslint/no-unnecessary-condition': 'off', // @typescript-eslint doesn't know if a field can changed by svelte, like an input field, so a field like export let isEnabled: boolean = true; will always be seen as just true by the linter.
 			},
 		},
 	],
