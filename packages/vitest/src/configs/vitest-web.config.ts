d5856f0ab7c97351aa24e75209e03fb69b4b0ad4
AlexAegis <alexaegis@gmail.com>
2023-07-19 22:35:05 +0200

    feat(vitest): add an include glob

--- packages/vitest/src/configs/vitest-web.config.ts
+++ packages/vitest/src/configs/vitest-web.config.ts
@@ -4,6 +4,7 @@ import { vitestCoverage } from './base-vitest.config.js';
 export const vitestWebConfig = {
 	plugins: [],
 	test: {
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 		globals: true,
 		environment: 'jsdom',
 		coverage: vitestCoverage,
