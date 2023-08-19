0ecb0bb949010c47178a31d49cdd54adbc3bc3be
AlexAegis <alexaegis@gmail.com>
2023-08-19 17:41:58 +0200

    chore: fix format

--- apps/svelte-example-app/src/routes/+page.svelte
+++ apps/svelte-example-app/src/routes/+page.svelte
@@ -7,7 +7,7 @@
 	<header class="card-header">
 		<h1 class="h1">
 			<span
-				class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
+				class="bg-gradient-to-br from-blue-500 to-cyan-300 box-decoration-clone bg-clip-text text-transparent"
 			>
 				Example App
 			</span>
