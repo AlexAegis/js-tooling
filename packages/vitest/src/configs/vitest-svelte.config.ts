23ae3646f7d6273eb56b6741c01abd18d50fa841
AlexAegis <alexaegis@pm.me>
2024-05-20 13:31:44 +0200

    feat(vitest): add the svelte testing library

--- packages/vitest/src/configs/vitest-svelte.config.ts
+++ packages/vitest/src/configs/vitest-svelte.config.ts
@@ -1,7 +1,12 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { mergeConfig } from 'vitest/config';
 import { vitestWebConfig } from './vitest-web.config.js';
 
+/**
+ * This vitest config contains the svelte plugin from @sveltejs/vite-plugin-svelte
+ * and the svelteTesting plugin from @testing-library/svelte/vite
+ */
 export const vitestSvelteConfig = mergeConfig(vitestWebConfig, {
-	plugins: [svelte({ hot: !process.env['VITEST'] })],
+	plugins: [svelte({ hot: !process.env['VITEST'] }), svelteTesting()],
 });
